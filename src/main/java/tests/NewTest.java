3ef1cfa1eae8f7eb6afaddcec13af37685e20bf5
Vikas Ghadage <Vikas.ghadage@everseen.com>
2019-12-26 10:45:05 +0530

    updated

--- src/main/java/tests/NewTest.java
+++ src/main/java/tests/NewTest.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+/*import org.openqa.selenium.chrome.ChromeDriver;*/
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
